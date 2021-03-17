a0f0b6f1bc8b1da66f0de0ca75c25213f2e910bb
Al-Azam <al.azam@students.amikom.ac.id>
2021-03-17 07:24:04 +0700

    Staging file Tugas Pertemuan 3

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Pertemuan_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat Objek dari Class Taxi
+            TAXI A = new TAXI();
+            TAXI B = new TAXI();
+            TAXI C = new TAXI();
+
+            //Mengatur Data Objek A, B, dan C
+            A.Name = "Johny";
+            A.Duty = "OFF";
+            A.Sum = 0;
+
+            B.Name = "Lin Tan Huang";
+            B.Duty = "ON";
+            B.Sum = 4;
+
+            C.Name = "Kyujin Lee";
+            C.Duty = "ON";
+            C.Sum = 2;
+
+            //Memanggil method
+            Console.WriteLine("\n");
+            A.Info();
+            A.Drop();
+            Console.WriteLine("\n");
+            B.Info();
+            B.PickUp();
+            Console.WriteLine("\n");
+            C.Info();
+            C.PickUp();
+            Console.WriteLine("\n");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TAXI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Pertemuan_3
+{
+    public class TAXI
+    {
+        //Properties
+        public string Name { get; set; }
+        public string Duty { get; set; }
+        public int Sum { get; set; }
+
+        //Method
+        public void Info()
+        {
+            Console.WriteLine("Driver = {0}", Name);
+            Console.WriteLine("Duty = {0}", Duty);
+            Console.WriteLine("Passanger = {0}", Sum);
+        }
+
+        public void PickUp() 
+        {
+            Console.WriteLine("He is still picking up Passangers");
+        }
+
+        public void Drop()
+        {
+            Console.WriteLine("He has droped off the passangers");
+        }
+    }
+}
